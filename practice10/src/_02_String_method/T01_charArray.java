fd6dcbdb88f30ed8e26365344a94ffcce8577b95
YunWooJin <satoru31572@gmail.com>
2025-08-14 21:45:00 +0900

    예제 클리어

--- practice10/src/_02_String_method/T01_charArray.java
+++ practice10/src/_02_String_method/T01_charArray.java
@@ -1,5 +1,6 @@
 package _02_String_method;
 
+
 public class T01_charArray {
 
 	public static void main(String[] args) {
@@ -12,6 +13,16 @@ public class T01_charArray {
 		
 		char ch = str3.charAt(1);
 		System.out.println(ch);
+		
+		// 주민번호 넣을 변수 하나 지정하고
+		// 여기서 8번째가 1,3이면 남자, 2,4이면 여자
+		String resi = new String("150315-3254208");
+		
+		System.out.println("주민번호 : " + resi);
+		
+		char gen_num = resi.charAt(7);
+		if(gen_num == '1' || gen_num == '3') System.out.println("당신의 성별은 남성이다.");
+		else System.out.println("당신의 성별은 여성이다.");
 	}
 
 }
